46b7d4534bdf15d8ec909975b4bdf7a9bda15cb2
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 19:41:44 +0800

    Update 1pt.sh

--- src/1pt.sh
+++ src/1pt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 api="https://csclub.uwaterloo.ca/~phthakka/1pt"
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 
 function add_url() {
 	curl --request POST \
 		--url "$api/addURL" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "accept: application/json"	\
 		--header "content-type: application/json" \
 		--data '{"url": "'$1'"}'
